f33e8fbdc1fac11e9cfcc89eae763052db49b810
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-12-15 05:35:05 -0500

    added model parameters

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,18 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-
+from rest_framework import response
 #from djongo import models as djongomodels
 
 #from enumchoicefield import EnumChoiceField
 
 # Create your models here.
+experience_choices=[("BEGINNER","Beginner"),("INTERMEDIATE","Intermediate"),("EXPERT","Expert")]
+language_choices=[("NONE","None"),("JAVA","Java"),("JS","Js"),("PYTHON","Python"),("HTML/CSS","Html/css"),
+				  ("C++","C++")]
+subject_areas=[("COMPUTER VISION","computer vision"),("REACT","react"),
+			   ("MACHINE LEARNING","Machine learning"),("FUNDAMENTALS","Fundamentals"),
+			   ("NLP","Nlp"),("AUGMENTED REALITY","Augmented reality"),("VIRTUAL REALITY","Virtual reality")]
 class Tags(models.Model):
 	tag_name = models.CharField(max_length=50)
 
@@ -15,9 +21,9 @@ class Tags(models.Model):
 
 class Resource(models.Model):
 	name = models.CharField(max_length=64)
-	link = models.TextField(max_length=500)
-	experience = models.CharField(max_length=32,choices=[("beginner","BEGINNER")],blank=True)
-	time = models.CharField(max_length=32,choices=[("brief","BRIEF")],blank=True)
+	link = models.URLField(max_length=500)
+	experience = models.CharField(max_length=32,choices=experience_choices,blank=True)
+	time = models.CharField(max_length=32,choices=[("BRIEF","Brief")],blank=True)
 	
 	topics = models.ManyToManyField(Tags,related_name='topics')
 	focus = models.ManyToManyField(Tags,related_name='focus')
@@ -29,8 +35,18 @@ class Resource(models.Model):
 class Course(models.Model):
 	name = models.CharField(max_length=64)
 	resources = models.ManyToManyField(Resource,related_name='courses')
-
+class Interest(models.Model):
+	interest = models.CharField(max_length=64, choices=subject_areas)
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	enrolled = models.ManyToManyField(Course,related_name='enrolled')
+	experience = models.CharField(max_length=64,choices=experience_choices)
+	language = models.CharField(max_length=20,choices=language_choices)
+	depth= models.CharField(max_length=30)
+	interests=models.ManyToManyField(Interest,related_name='interests')
+	timeCommitment=models.DurationField()
+
+
+
+
 
