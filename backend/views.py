481ea139e7b52cda79eb1b6254abc0ceebdce98e
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-12-15 02:21:19 -0500

    authenticate done

--- backend/views.py
+++ backend/views.py
@@ -10,14 +10,11 @@ from . serializers import *
 class TagsView(viewsets.ModelViewSet):
 	queryset = Tags.objects.all()
 	serializer_class = TagsSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class ResourceView(viewsets.ModelViewSet):
 	queryset = Resource.objects.all()
 	serializer_class = ResourceSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class CourseView(viewsets.ModelViewSet):
 	queryset = Course.objects.all()
 	serializer_class = CourseSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
