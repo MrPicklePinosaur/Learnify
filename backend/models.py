80c70cd0c735e6bf79deb31a421437a547f89db1
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-12-14 23:00:01 -0500

    got rid of enums

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,13 @@
 from backend.enums import *
 from django.db import models
-from enumchoicefield import EnumChoiceField
+#from enumchoicefield import EnumChoiceField
 
 # Create your models here.
 class Resource(models.Model):
 	name = models.CharField(max_length=64)
 	link = models.TextField(max_length=500)
-	#experience = models.CharField(max_length=32,choices=[(tag,tag.value) for tag in enums.Experience])
-	#time = models.CharField(max_length=32,choices=[(tag,tag.value) for tag in enums.TimeToComplete])
-	experience = EnumChoiceField(enum_class=Experience, default=Experience.python)
-	time = EnumChoiceField(enum_class=TimeToComplete, default=TimeToComplete.brief)
+	experience = models.CharField(max_length=32,choices=[("beginner","BEGINNER")],blank=True)
+	time = models.CharField(max_length=32,choices=[("brief","BRIEF")],blank=True)
 	'''
 	topics = 
 	focus = 
